da004ab9631be90e2aded4b7fcac35c272d45fbf
JavierVelascoGalicia <javiervelasco0109@gmail.com>
2025-09-05 14:21:25 -0600

    Add init.sql with the tables to be created and add health check to db

---
+++ db-scripts/init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY,created_at TIMESTAMPTZ DEFAULT NOW());
+
+CREATE TABLE IF NOT EXISTS wallets (
+    wallet_id SERIAL PRIMARY KEY,
+    user_id INT,
+    balance DECIMAL DEFAULT 0.00,
+    currency CHAR(3),
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id));
+
+CREATE TABLE IF NOT EXISTS transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    wallet_id INT,
+    user_id INT,
+    amount DECIMAL,
+    currency CHAR(3),
+    transaction_type INT,
+    transaction_date TIMESTAMPTZ DEFAULT NOW(),
+    
+    CONSTRAINT fk_wallet FOREIGN KEY (wallet_id) REFERENCES wallets(wallet_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id));
+
